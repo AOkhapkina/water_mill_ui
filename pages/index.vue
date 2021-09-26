2bc50ba9d8012f401ff6c9aa5ab4ac7d58a03168
Alina <K.alinka555@list.ru>
2021-09-26 23:10:50 +0300

    Добавлены все файлы проекта (front and back). Находится в процессе написания.

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,20 @@
     <b-row>
       <b-col style="padding: 5%;">
         <ul class="tools">
-          <li class="water">
-            <b-badge variant="primary"></b-badge>
-          </li>
+          <li class="water"><b-badge variant="primary">0</b-badge></li>
           <li class="txt">+</li>
-          <li class="millet">
-            <b-badge variant="warning"></b-badge>
-          </li>
+          <li class="millet"><b-badge variant="warning"></b-badge></li>
           <li class="txt">=</li>
-          <li class="flour">
-            <b-badge variant="primary"></b-badge>
-          </li>
+          <li class="flour"><b-badge variant="primary"></b-badge></li>
         </ul>
+        <div>
+          <h3>Water: 0</h3>
+          <b-progress :value="3" :max="10" animated variant="primary"></b-progress>
+          <h3>Energy: 0</h3>
+          <b-progress :value="4" :max="10" animated variant="warning"></b-progress>
+          <h3>Millet: 0</h3>
+          <b-progress :value="5" :max="10" animated variant="info"></b-progress>
+        </div>
       </b-col>
     </b-row>
   </b-container>
@@ -40,12 +42,12 @@
   font-size: 200%;
 }
 .tools li.water {
-  background: #fff  no-repeat center center/cover;
+  background: #fff url(/water.png)  no-repeat center center/cover;
 }
 .tools li.millet {
-  background: #fff  no-repeat center center/cover;
+  background: #fff url(/millet.png) no-repeat center center/cover;
 }
 .tools li.flour {
-  background: #fff  no-repeat center center/cover;
+  background: #fff url(/flour.png) no-repeat center center/cover;
 }
 </style>
